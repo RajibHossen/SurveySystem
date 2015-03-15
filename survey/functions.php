8be84ade1660fa654ef24e285fc4a53b6e15e96c
RajibHossen <rajibcse2k10@gmail.com>
2015-03-15 14:46:23 +0600

    Solve the table rowspan problem. Now questions are fetching according to category names.

--- survey/functions.php
+++ survey/functions.php
@@ -53,32 +53,63 @@ function printSurvey()
 	<form method="POST" action="">
 	<input type="hidden" name="sid" value="">
 	<input type="hidden" name="action" value="0">';
-	
-	$result = mysqli_query($sql, "SELECT * from questions GROUP BY category");
 
-	while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
+	$test = mysqli_query($sql,"SELECT category,count(*) AS value FROM question GROUP BY category");
+
+	while ($row = mysqli_fetch_array($test, MYSQL_ASSOC)) {
+		# code...
+		$categoryNames[] = $row['category'];
+		$values[] = $row['value'];
+	}
+	$a = 0;
+	$i = 1;
+	foreach ($categoryNames as $cat) {
+
+		$result = mysqli_query($sql, "SELECT question_id,question from question WHERE category ='$cat'");
+
+		while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) 
 		{
-			$quesionID[] =  $row['question_id'];
-			$category[] = $row['category'];
+			$questionID[] =  $row['question_id'];
 			$question[] = $row['question'];
 
 		}
 		$count = 0;
-		foreach ($quesionID as $questionsID) {
+		$increment = 0;
+		for($increment = 0;$increment < $values[$a];$increment++) {
+			if($count==0)
+			{
 				echo '<tr>';
-				echo '<td>'.$category[$count].'</td>';
-				echo '<td>'.$count.'</td>';
+				echo '<td rowspan=$values[$a] style="border-bottom:none">'.$cat.'</td>';
+				echo '<td>'.$i.'</td>';
 				echo '<td>'.$question[$count].'</td>';
-				echo '<td align="center"><input type="radio" name="'.$questionsID.'" value="5"></td>';
-				echo '<td align="center"><input type="radio" name="'.$questionsID.'" value="4"></td>';
-				echo '<td align="center"><input type="radio" name="'.$questionsID.'" value="3"></td>';
-				echo '<td align="center"><input type="radio" name="'.$questionsID.'" value="2"></td>';
-				echo '<td align="center"><input type="radio" name="'.$questionsID.'" value="1"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="5"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="4"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="3"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="2"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="1"></td>';
 				echo '</tr>';
-				$count++;
+			}
+			else
+			{
+				echo '<tr>';
+				echo '<td style="border:none"></td>';
+				echo '<td>'.$i.'</td>';
+				echo '<td>'.$question[$count].'</td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="5"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="4"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="3"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="2"></td>';
+				echo '<td align="center"><input type="radio" name="'.$questionID[$count].'" value="1"></td>';
+				echo '</tr>';
 
-			}	
-
+			}
+			$count++;
+			$i++;
+			}
+		$a++;
+		# code...
+	}
+	
 	echo '</form></table></div>';
 	echo '<div id="submit"><input type="submit" value="Submit"></div>';	
 	echo mysqli_error($sql);
